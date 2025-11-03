f228d08216e3759cfc3b2ff14bb648fcea3b7eed
Yousef Assem <usefassem96@gmail.com>
2025-11-03 14:46:06 +0200

    service cards turned into slider

--- app/_components/Services/Services.jsx
+++ app/_components/Services/Services.jsx
@@ -1,8 +1,12 @@
+'use client'
 import { ArrowRight } from "lucide-react";
 import AboutSlider from "../About/AboutSlider";
-import Image from "next/image";
+import ServicesSlider from "./ServicesSlider";
+import { useRef } from "react";
 
 function Services() {
+const sliderRef = useRef(null)
+
   const servicesData = [
     {
       title: "UX Design",
@@ -19,21 +23,67 @@ function Services() {
       description:
         "High quality iOS applications that provide seamless user experiences.",
     },
+    {
+      title: "UX Design",
+      description:
+        "UX Design services with focus on creating intuitive user experiences.",
+    },
+    {
+      title: "Android Apps",
+      description:
+        "Custom Android apps built to meet specific goals and user needs.",
+    },
+    {
+      title: "iOS Apps",
+      description:
+        "High quality iOS applications that provide seamless user experiences.",
+    },
+    {
+      title: "UX Design",
+      description:
+        "UX Design services with focus on creating intuitive user experiences.",
+    },
+    {
+      title: "Android Apps",
+      description:
+        "Custom Android apps built to meet specific goals and user needs.",
+    },
+    {
+      title: "iOS Apps",
+      description:
+        "High quality iOS applications that provide seamless user experiences.",
+    },
+    {
+      title: "UX Design",
+      description:
+        "UX Design services with focus on creating intuitive user experiences.",
+    },
+    {
+      title: "Android Apps",
+      description:
+        "Custom Android apps built to meet specific goals and user needs.",
+    },
   ];
+
+      const handleNext = () => {
+        sliderRef.current?.slickNext()
+    }
 
   return (
     <>
       <div className="relative bg-[#002733]" id="services">
       <div className="overlay absolute inset-0 opacity-80 asfalt-overlay"></div>
       <div className="section-background z-10 opacity-55"></div>
-      <section className="relative  px-6 md:px-12 lg:px-20 py-16 md:py-24 overflow-hidden">
+      <section className="relative px-6 md:px-12 lg:px-20 py-16 md:py-24 overflow-hidden">
         <div className="absolute top-1/2 right-[-150px] -translate-y-1/2 w-[400px] h-[400px] rounded-full 
                         bg-linear-to-l from-white/60 to-transparent blur-3xl opacity-70 pointer-events-none">
         </div>
         <div className="absolute top-1/2 left-[-180px] -translate-y-1/2 w-[400px] h-[400px] rounded-full 
                         bg-linear-to-l from-white/60 to-transparent blur-3xl opacity-50 pointer-events-none">
         </div>
+
           <div className="mx-auto ">
+            
             {/* Header */}
             <div className="flex flex-col md:flex-row justify-between w-full items-start md:items-center mb-12 gap-6 md:gap-0">
               <div>
@@ -44,44 +94,15 @@ function Services() {
                   Your Software Partner
                 </h2>
               </div>
-              <button className="flex items-center gap-4 text-xl text-[#F5F4EE] rounded-full cursor-pointer font-[Salmond-medium]">
+              <button onClick={handleNext} className="flex items-center gap-4 text-xl text-[#F5F4EE] rounded-full cursor-pointer font-[Salmond-medium]">
                 All Service
                 <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center hover:bg-green-400 transition-all duration-200">
                   <ArrowRight className="w-5 h-5 text-black" />
                 </div>
               </button>
             </div>
-
-            {/* Service Cards */}
-<div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 lg:grid-cols-3 justify-items-center mb-16">
-    {servicesData.map((service, index) => (
-      <div key={index} className="relative mt-10 w-full max-w-[400px] lg:max-w-[421px] h-[330px]">
-        <div
-          className="absolute inset-0 group bg-[#E9EAE533] border-2 border-transparent rounded-[30px] p-8 text-[#F5F4EE] 
-                    transition-all duration-300 will-change-transform origin-bottom 
-                    hover:scale-y-120 hover:bg-[#89EB934D] hover:border-[#76F483]"
-        >
-          <div className="transition-transform duration-300 group-hover:scale-y-[0.833]">
-            <h3 className="text-3xl font-[Salmond-medium] mb-4 transition-colors duration-300 group-hover:text-[#58F468]">
-              {service.title}
-            </h3>
-            <p className="text-xl w-75 mb-24 leading-[140%] font-[Salmond-news] text-[#F5F4EE]">{service.description}</p>
-          </div>
-          <div className="transition-transform duration-300 group-hover:scale-y-[0.833]">
-            <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center transition-all duration-300 group-hover:bg-[#58F468] group-hover:-rotate-45">
-              <ArrowRight className="w-5 h-5 text-[#1C1B1F] transition-all duration-300" />
-            </button>
-          </div>
-        </div>
-      </div>
-    ))}
-</div>
-
-
-
-            {/* Technology Logos Slider */}
-              <AboutSlider />
-
+            <ServicesSlider servicesData = {servicesData} sliderRef={sliderRef} />
+            <AboutSlider />
           </div>
         </section>
       </div>
---
+++ app/_components/Services/ServicesSlider.jsx
@@ -0,0 +1,65 @@
+import { ArrowRight } from "lucide-react"
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import React, { useRef } from "react";
+function ServicesSlider({servicesData , sliderRef}) {
+      
+    const swiperRef = useRef(null)
+      React.useImperativeHandle(sliderRef, () => ({
+        slickNext: () => swiperRef.current?.slideNext(),
+      }))
+
+  return (
+            <Swiper
+              modules={[Autoplay]}
+              slidesPerView={3}
+              spaceBetween={26}
+            //   autoplay={{
+            //     delay: 3500,
+            //     disableOnInteraction: false
+            //   }}
+              loop={true}
+              speed={500}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              breakpoints={{
+                0: { slidesPerView: 1.2, spaceBetween: 15 },
+                768: { slidesPerView: 2, spaceBetween: 20 },
+                1280: { slidesPerView: 3, spaceBetween: 26 },
+              }}
+              className="mb-16"
+            >
+              {servicesData.map((service, index) => (
+                <SwiperSlide key={index}>
+                  <div className="relative mt-17 w-full max-w-[421px] h-[330px]">
+                    <div
+                      className="absolute inset-0 group bg-[#E9EAE533] border-2 border-transparent rounded-[30px] p-8 text-[#F5F4EE] 
+                                transition-all duration-300 will-change-transform origin-bottom 
+                                hover:scale-y-120 hover:bg-[#89EB934D] hover:border-[#76F483]"
+                    >
+                      <div className="transition-transform duration-300 group-hover:scale-y-[0.833]  w-full">
+                        <h3 className="md:text-3xl text-2xl font-[Salmond-medium] mb-4 transition-colors duration-300 group-hover:text-[#58F468]">
+                          {service.title}
+                        </h3>
+                        <p className="md:text-xl text-lg lg:w-75 mb-24 leading-[140%] font-[Salmond-news] text-[#F5F4EE]">
+                          {service.description}
+                        </p>
+                      </div>
+
+                      <div className="transition-transform absolute bottom-4 duration-300 group-hover:scale-y-[0.833]">
+                        <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center transition-all duration-300 group-hover:bg-[#58F468] group-hover:-rotate-45">
+                          <ArrowRight
+                            className="w-5 h-5 text-[#1C1B1F] transition-all duration-300"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+  )
+}
+
+export default ServicesSlider
