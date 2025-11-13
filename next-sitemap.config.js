49bf8f12be12120e76b5fc0358e079bbe798cef3
Yousef Assem <usefassem96@gmail.com>
2025-11-13 13:34:48 +0200

    Added: sitemap and robots files

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://farapi-website-phi.vercel.app/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
+
+export default config;
