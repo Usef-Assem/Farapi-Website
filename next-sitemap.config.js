ccb6d57e7675d257b075bfcec57dd57e981a24be
Yousef Assem <usefassem96@gmail.com>
2025-11-14 02:27:32 +0200

    regenerate sitemap files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,11 @@ const config = {
   siteUrl: 'https://farapi-website-phi.vercel.app/',
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  exclude: [
+    '/icon.svg',
+    '/apple-icon*',
+    '/favicon.ico',
+  ],
 };
 
 export default config;
