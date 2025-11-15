a318852f3ebf8a3f8679e1cad4278db22be40109
Yousef Assem <usefassem96@gmail.com>
2025-11-15 16:26:55 +0200

    fixed: next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: 'https://farapi-website-phi.vercel.app', // Remove trailing slash
+module.exports = {
+  siteUrl: 'https://farapi-website-phi.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  generateIndexSitemap: false, // Add this - prevents sitemap index
+  generateIndexSitemap: false,
   exclude: [
     '/icon.svg',
     '/apple-icon*',
     '/favicon.ico',
   ],
 };
-
-export default config;
